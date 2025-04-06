69a40dcdf3861f5aefabc18005f7cedf0d59dab2
Mungstrocity <jarett@mungstrocity.com>
2025-04-06 14:49:47 -0500

    posting working yolo8 files in their own directory

--- fgvc_training.py
+++
@@ -1,9 +0,0 @@
-import kagglehub
-import os
-
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-custom_path = os.path.join(BASE_DIR, "dataset")
-
-path = kagglehub.dataset_download("seryouxblaster764/fgvc-aircraft", target_dir=custom_path)
-
